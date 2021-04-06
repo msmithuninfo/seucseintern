1d72663c50218476b0ae6c59b7518cde286ce583
msmithuninfo <msmithuninfo@gmail.com>
2021-04-07 02:55:37 +0600

    mithun menu

---
+++ js/mySidebarMenu.js
@@ -0,0 +1,131 @@
+
+
+function myFunctionDropdown1() {
+  var x = document.getElementById("SideDropMenu1");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+
+  }
+}
+
+
+function myFunctionDropdown2() {
+  var x = document.getElementById("SideDropMenu2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myFunctionDropdown3() {
+  var x = document.getElementById("SideDropMenu3");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myFunctionDropdown4() {
+    var x = document.getElementById("SideDropMenu4");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myFunctionDropdown5() {
+    var x = document.getElementById("SideDropMenu5");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+  function myFunctionDropdown6() {
+    var x = document.getElementById("SideDropMenu6");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myFunctionDropdown7() {
+    var x = document.getElementById("SideDropMenu7");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+    function rotateArrow1() {
+        var okk1 = document.getElementById('activeArrow1');
+        if (okk1.style.transform === "rotate(90deg)") {
+          okk1.style.transform = "rotate(0deg)";
+        } else {
+          okk1.style.transform = "rotate(90deg)";
+        }
+    }
+
+    function rotateArrow2() {
+        var okk1 = document.getElementById('activeArrow2');
+        if (okk1.style.transform === "rotate(90deg)") {
+          okk1.style.transform = "rotate(0deg)";
+        } else {
+          okk1.style.transform = "rotate(90deg)";
+        }
+    }
+
+    function rotateArrow3() {
+        var okk1 = document.getElementById('activeArrow3');
+        if (okk1.style.transform === "rotate(90deg)") {
+          okk1.style.transform = "rotate(0deg)";
+        } else {
+          okk1.style.transform = "rotate(90deg)";
+        }
+    }
+
+    function rotateArrow4() {
+        var okk1 = document.getElementById('activeArrow4');
+        if (okk1.style.transform === "rotate(90deg)") {
+          okk1.style.transform = "rotate(0deg)";
+        } else {
+          okk1.style.transform = "rotate(90deg)";
+        }
+    }
+
+    function rotateArrow5() {
+        var okk1 = document.getElementById('activeArrow5');
+        if (okk1.style.transform === "rotate(90deg)") {
+          okk1.style.transform = "rotate(0deg)";
+        } else {
+          okk1.style.transform = "rotate(90deg)";
+        }
+    }
+
+    function rotateArrow6() {
+        var okk1 = document.getElementById('activeArrow6');
+        if (okk1.style.transform === "rotate(90deg)") {
+          okk1.style.transform = "rotate(0deg)";
+        } else {
+          okk1.style.transform = "rotate(90deg)";
+        }
+    }
+
+    function rotateArrow7() {
+        var okk1 = document.getElementById('activeArrow7');
+        if (okk1.style.transform === "rotate(90deg)") {
+          okk1.style.transform = "rotate(0deg)";
+        } else {
+          okk1.style.transform = "rotate(90deg)";
+        }
+    }
